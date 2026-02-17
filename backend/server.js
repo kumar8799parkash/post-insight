1fcd5f6ba96d5a77cf3af69c74563c886f059b49
kumar8799parkash <kumar8799parkash@gmail.com>
2026-02-17 08:53:47 +0530

    modified server.js for render

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import analyzeRouter from './routes/analyzeRoutes.js';
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 /* app.use(cors()); */
 app.use(cors({ origin: "*" }));
@@ -17,8 +18,8 @@ app.get('/' , (req ,res)=>{
 
 app.use('/api' , analyzeRouter);
 
-app.listen(process.env.PORT , ()=>{
-    console.log(`App is listening on port - ${process.env.PORT}`);
+app.listen(PORT , ()=>{
+    console.log(`App is listening on port - ${PORT}`);
 })
 
 
